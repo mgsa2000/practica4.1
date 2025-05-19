69b90f14575f1db51fc2fcaa92c0e2b04bc4f957
Miguel Ángel Guerrero-Strachan Alcoba <stratachan3@gmail.com>
2025-05-19 17:29:10 +0200

    Update 04-create_instances.sh

--- ejercicio6/04-create_instances.sh
+++ ejercicio6/04-create_instances.sh
@@ -1,1 +1,59 @@
+#!/bin/bash
+set -x
 
+# Deshabilitamos la paginación de la salida de los comandos de AWS CLI
+# Referencia: https://docs.aws.amazon.com/es_es/cli/latest/userguide/cliv2-migration.html#cliv2-migration-output-pager
+export AWS_PAGER=""
+
+# Variables de configuración
+source .env
+
+# Creamos una intancia EC2 para el frontend 1
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP_FRONTEND \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_FRONTEND-1}]"
+    --user-data "sudo apt update && sudo apt upgrade -y"
+
+# Creamos una intancia EC2 para el frontend 2
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP_FRONTEND \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_FRONTEND-2}]"
+    --user-data "sudo apt update && sudo apt upgrade -y"
+
+# Creamos una intancia EC2 para el backend
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP_BACKEND \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_BACKEND}]"
+    --user-data "sudo apt update && sudo apt upgrade -y"
+
+# Creamos una intancia EC2 para el loadbalancer
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP_LB \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_LB}]"
+    --user-data "sudo apt update && sudo apt upgrade -y"
+
+# Creamos una intancia EC2 para el sevidor NFS
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP_NFS \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_NFS}]"
+    --user-data "sudo apt update && sudo apt upgrade -y"
